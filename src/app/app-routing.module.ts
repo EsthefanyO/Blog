81643deb6f51687311681ba7eb582a43a94d4990
Esthefany07 <teffarr@gmail.com>
2020-08-16 10:27:59 +0200

    Rutas y modelo Post creados.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/Blog' },
+  { path: 'Blog', component: BlogComponent },
+  { path: 'Newpost', component: FormularioComponent },
+  { path: '**', redirectTo: '/Blog' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
